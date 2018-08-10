a2e0b85f6c7e5efbb8b304bf7e8b3bd2346e8249
darkmodz <42271443+darkmodz@users.noreply.github.com>
2018-08-10 17:40:57 +0530

    Add files via upload

---
+++ gg.lua
@@ -0,0 +1,66 @@
+gg.alert("Test Script")
+
+function menu()
+mn = gg.choice({
+	'🏃Super Speedhack🏃',
+	'Bypass',
+	'EXIT'},
+	 nil,'🦇Dark-Modz🦇')
+if mn == 1 then SPDA() end
+if mn == 2 then A() end
+if mn == 3 then EXIT() end
+HOMEDM=-1
+end
+
+
+function A()
+gg.toast("BYPASS IS LOADING")
+gg.searchNumber("1.2F;1.8F:9::1", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("1.1F;1.4F:3::1", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("1.1D;1.4D:2::10", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+gg.clearResults()
+gg.searchNumber("5001;1.1;1F::10", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(100)
+gg.searchNumber("1.2F;1.8F:9::1", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("1.1F;1.4F:3::1", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("1.1D;1.4D:2::10", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+gg.clearResults(850)
+gg.searchNumber("1.2F;1.8F:9::1", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("1.1F;1.4F:3::1", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("1.1D;1.4D:2::10", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+gg.clearResults()
+gg.searchNumber("5001;1.1;1F::10", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(100)
+gg.searchNumber("1.2F;1.8F:9::1", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("1.1F;1.4F:3::1", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("1.1D;1.4D:2::10", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+gg.clearResults(850)
+gg.toast("BAYPASS ACTIVE ON")
+end
+
+
+function SPDA() 
+  gg.clearResults()
+  gg.setRanges(gg.REGION_CODE_APP)
+  gg.searchNumber("10.90734863281;0.00999999978", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("10.90734863281", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(300)
+  gg.editAll("10.3111", gg.TYPE_FLOAT)
+  gg.toast("SPEED HACK LOADED")
+end
+
+function EXIT()
+gg.toast("Hope you liked it!")
+print(' script by Dark Modz   ')
+os.exit()
+end
+
+cs = ""
+while(true)
+do
+  if gg.isVisible(true) then
+    HOMEDM=1
+    gg.setVisible(false) 
+  end 
+  if HOMEDM==1 then menu() end
+end
